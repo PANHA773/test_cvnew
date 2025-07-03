a0440d80d4ca0d02f5e5bd8022a2fb62b75e5ce4
PANHA773 <147047545+PANHA773@users.noreply.github.com>
2025-07-03 13:09:57 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/test_profile/', // 👈 this is required for GitHub Pages!
   plugins: [react()],
+  base: "/panha-porfolio"
 })
